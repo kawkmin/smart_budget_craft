099bdc9b35d2f62de9173135ee15beee4a756236
kawksung <alstjd5479@naver.com>
2023-11-09 23:34:36 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/personal/smartbugetcraft/SmartBugetCraftApplication.java
@@ -0,0 +1,13 @@
+package com.personal.smartbugetcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBugetCraftApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartBugetCraftApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/personal/smartbugetcraft/SmartBugetCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.smartbugetcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBugetCraftApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
