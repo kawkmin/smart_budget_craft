6e5017f7af2cf2e439669c5580c4a9ab256fa263
kawksung <alstjd5479@naver.com>
2023-11-13 11:26:46 +0900

    sql: 비용 카테고리의 Init sql 파일 생성

---
+++ src/main/resources/sql/cost-category-init.sql
@@ -0,0 +1,21 @@
+insert into cost_category (cost_category_id, name)
+VALUES (null, "생활/마트");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "외식");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "의료/건강");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "학습/교육");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "주거/통신");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "금융/보험");
+
+insert into cost_category (cost_category_id, name)
+VALUES (null, "기타");
+
